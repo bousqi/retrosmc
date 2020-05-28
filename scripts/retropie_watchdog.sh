604af48e4af18a77d5e1a1bfdd10ec9fa17455c9
Bousqi <bousqi@gmail.com>
2020-05-28 17:44:26 +0200

    Updated scripts + cleaning

--- scripts/retropie_watchdog.sh
+++ scripts/retropie_watchdog.sh
@@ -2,18 +2,16 @@
 
 # Script by mcobit
 
-. /home/osmc/RetroPie/scripts/retrosmc-config.cfg
+. ~/RetroPie/scripts/retrosmc-config.cfg
 
 # ugly fix for people having trouble with CEC
-
 if [ "$CECFIX" = 1 ]; then
-sudo /usr/osmc/bin/cec-client -p 1 &
+sudo /usr/bin/cec-client -p 1 &
 sleep 1
 sudo kill -9 $(pidof cec-client)
 fi
 
 # deactivate the hyperion deamon if it is running
-
 if [ "$HYPERIONFIX" = 1 ]; then
    if [ $(pgrep hyperion) ]; then
       sudo service hyperion stop
@@ -21,11 +19,9 @@ if [ "$HYPERIONFIX" = 1 ]; then
 fi
 
 # give emulationstation time to start up
-
 sleep 8
 
 # activate hyperion daemon if it is not running
-
 if [ "$HYPERIONFIX" = 1 ]; then
    if [ ! $(pgrep hyperion) ]; then
       sudo service hyperion start
@@ -33,27 +29,22 @@ if [ "$HYPERIONFIX" = 1 ]; then
 fi
 
 # check for emulationstation running
-
 while [ true ]; do
 	VAR1="$(pgrep emulatio)"
 
-# if emulationstation is quit, clear the screen of virtual terminal 7 and show a message
+	# if emulationstation is quit, clear the screen of virtual terminal 7 and show a message
+	if [ ! "$VAR1" ]; then
+		sudo openvt -c 7 -s -f clear
 
-		if [ ! "$VAR1" ]; then
-			sudo openvt -c 7 -s -f clear
+		# restart kodi
+		# sudo su -c "rfkill block bluetooth"
+		sudo su -c "sudo systemctl restart kodi.service &" &
 
-# restart kodi
-
-	sudo su -c "sudo systemctl restart mediacenter &" &
-
-# exit script
-
-			exit
-		else
-
-# if emulationstation is still running, wait 2 seconds and check again (could probably be longer, but doesn't seem to impact performance by much)
-
-			sleep 2
-fi
+		# exit script
+		exit
+	else
+		# if emulationstation is still running, wait 2 seconds and check again (could probably be longer, but doesn't seem to impact performance by much)
+		sleep 2
+	fi
 done
 exit
